e3cbb57c0b3e1cf09a5b9d53f5c0d2893f00ab43
abhiSuman99 <131230123+abhiSuman99@users.noreply.github.com>
2024-05-02 02:21:53 +0530

    Add files via upload

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'; 
-import rootReducer from './rootReducer'; // Update this path with the correct path to your rootReducer file
+// Import the configureStore utility from Redux Toolkit
+import { configureStore } from '@reduxjs/toolkit';
+// Import your rootReducer; make sure the path is correct
+import rootReducer from './rootReducer';
 
-// Create the Redux store with thunk middleware applied
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// Create the Redux store using configureStore
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+    devTools: process.env.NODE_ENV !== 'production', // DevTools enabled conditionally based on environment
+});
 
+// Export the configured store
 export default store;
