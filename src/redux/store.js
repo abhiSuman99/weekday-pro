8b5d260d1e9e968cca6152c7bf89c8115fc2a4c4
abhiSuman99 <131230123+abhiSuman99@users.noreply.github.com>
2024-05-02 02:03:20 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk'; // Correct import statement for redux-thunk
+import thunk from 'redux-thunk'; 
 import rootReducer from './rootReducer'; // Update this path with the correct path to your rootReducer file
 
 // Create the Redux store with thunk middleware applied
